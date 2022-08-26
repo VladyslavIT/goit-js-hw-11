a548cf15225e0e5b9093781fd2b6fa89eef2e713
VladyslavIT <vlad.chernyy.1997@gmail.com>
2022-08-26 22:43:36 +0300

    a

--- src/api/fetchImages.js
+++ src/api/fetchImages.js
@@ -1,1 +1,10 @@
 import axios from "axios";
+
+export default async function fetchImages(q) {
+
+const BASE_URL = 'https://pixabay.com/api/';
+const KEY = '29531020-3b97d8056313c52b7859c1bca';
+const FILTERS = 'image_type=photo&orientation=horizontal&safesearch=true&page=1&per_page=40'
+ 
+    return fetch(`${BASE_URL}?key=${KEY}&q=${q}&${FILTERS}`).then(response => response.json())
+};
--- src/index.js
+++ src/index.js
@@ -1,3 +1,21 @@
 import axios from "axios";
 import Notiflix from "notiflix";
 import fetchImages from "./api/fetchImages";
+
+const inputEL = document.querySelector('.form-input');
+const buttonEl = document.querySelector('.form-button');
+
+const onSearchImages = event => {
+  const image = event.target.value.trim();
+
+  fetchImages(image)
+    .then(images => console.log(images))
+    .catch(error => {
+      if (error.status === 404) {
+        Notiflix.Notify.failure();
+      }
+    });
+};
+
+inputEL.addEventListener('input', onSearchImages);
+
